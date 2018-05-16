bcc482471f79444b448720d525e4d84eed263c18
neeraj-dixit <neeraj.dixit@nutanix.com>
2018-05-16 19:46:24 +0530

    setup commit

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var jwt_util = {
+  getToken: function getToken() {},
+  validateToken: function validateToken() {},
+  decodeToken: function decodeToken() {}
+};
+
+exports.default = jwt_util;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+const jwt_util = {
+  getToken: () => {
+
+  },
+  validateToken: () => {
+
+  },
+  decodeToken: () => {
+    
+  }
+};
+
+export default jwt_util;
